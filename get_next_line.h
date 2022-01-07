71c91dc6b293307861814cfd8b2c92567d1a0b86
Pierre-alban Waters <pwaters@c1r6p12.hive.fi>
2022-01-07 14:48:56 +0200

    first version of gnl.c, gnl.h

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pwaters <pwaters@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/07 11:07:26 by pwaters           #+#    #+#             */
+/*   Updated: 2022/01/07 14:47:32 by pwaters          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFF_SIZE 32
+# define MAX_FD 4096 + 1
+int	get_next_line(const int fd, char **line);
+
+#endif
