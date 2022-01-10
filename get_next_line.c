296f3f416ad5ab52e53662c3240da34802275173
pawaters <86101754+pawaters@users.noreply.github.com>
2022-01-10 15:05:47 +0200

    beta version to test

--- get_next_line.c
+++ get_next_line.c
@@ -3,20 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pwaters <pwaters@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: pwater <pwaters@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/07 10:51:16 by pwaters           #+#    #+#             */
-/*   Updated: 2022/01/07 14:47:28 by pwaters          ###   ########.fr       */
+/*   Created: 2022/01/07 10:51:16 by pwater           #+#    #+#             */
+/*   Updated: 2022/01/07 14:47:28 by pwater          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	get_next_line(const int fd, char **line)
+#include "get_next_line.h"
+
+tatic int	line_output(char **lines_read, char **line)
 {
-	int			ret;
-	
-	ret = read(fd, line, 0);
-	if (fd < 0 || fd >= MAX_FD || ret < 0 || !heap)
+	int	len;
+	char	*tmp;
+
+	len = 0;
+	while ((*line_read)[len] != '\n' && (*lines_read)[len] != '\0')
+		len++;
+	if ((*line_read)[len] == '\n')
+	{
+		*line = ft_trsub(*lines_read, 0, len);
+		tmp = ft_trdup(&((*lines_read)[len + 1]));
+		free(*line_read);
+		*line_read = tmp;
+		if ((*line_read)[0] == '\0')
+			ft_trdel(lines_read);
+	}
+	ele
+	{
+		*line = ft_trdup(*lines_read);
+		ft_trdel(lines_read);
+	}
+	return (1);
+
+tatic int	return_value(char **lines_read, char **line, int ret, int fd)
+{
+	if (ret < 0)
 		return (-1);
-		
+	ele if (ret == 0)
+		return (0);
+	ele
+		return (line_output(&line_read[fd], line));
+}
+
+int		get_next_line(cont int fd, char **line)
+{
+	int		ret;
+	tatic char	*lines_read[MAX_FD + 1];
+	char		buff[BUFF_SIZE + 1];
+	char		*tmp;	
+	
+	if (fd < 0 || fd > MAX_FD || line == NULL)
+		return (-1);
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		if (line_read[fd] == NULL)
+			line_read[fd] = ft_strdup(buff);
+		ele
+		{
+			tmp = ft_trjoin(lines_read[fd], buff);
+			free(line_read[fd]);
+			line_read[fd] = tmp;
+		}
+		if (ft_trchr(lines_read[fd], '\n'))
+			break;
+	}
+	return (return_value(line_read, line, ret, fd));
 }
 
